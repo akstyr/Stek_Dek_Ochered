c21cb0b0295bb1895fcabfa8c9e7316e9ebed9f7
Kate_aks <katya.grohotova.4@gmail.com>
2018-04-27 02:02:50 +0700

    +Hannoy

--- Stek_Dek_Ochered/Stek_Dek_ochered/Stek_Dek_ochered.cpp
+++ Stek_Dek_Ochered/Stek_Dek_ochered/Stek_Dek_ochered.cpp
@@ -6,23 +6,125 @@
 #include "iostream"
 using namespace std;
 
+void Hannoi(int n, Stek <int> A, Stek <int> B, Stek <int> C)
+{ 
+	int i, x, a, b;
+	for (i = 0; i < (pow(2, n)); i++)
+	{
+		A.Print();
+		B.Print();
+		C.Print();
+		cout << endl;
+		if ((A.top != NULL) && (A.top->Data == 1))
+			x = 1;
+		if (B.top != NULL && B.top->Data == 1)
+			x = 2;
+		if (C.top != NULL && C.top->Data == 1)
+			x = 3;
+		if (i % 2 == 0)
+		{
+			if (x == 1)
+			{
+				a = A.Get();
+				C.Add(a);
+			}
+			if (x == 2)
+			{
+				a = B.Get();
+				A.Add(a);
+			}
+			if (x == 3)
+			{
+				a = C.Get();
+				B.Add(a);
+			}
+		}
+		else
+		{
+			if (x == 1)
+			{
+				a = B.Get();
+				b = C.Get();
+				if (b == 0)
+					C.Add(a);
+				else if (a == 0)
+					B.Add(b);
+				else if (a < b && b != 0)
+				{
+					C.Add(b);
+					C.Add(a);
+				}
+				else if (a > b && a != 0)
+				{
+					B.Add(a);
+					B.Add(b);
+				}
+			}
+			else if (x == 2)
+			{
+				a = A.Get();
+				b = C.Get();
+				if (b == 0)
+					C.Add(a);
+				else if (a == 0)
+					A.Add(b);
+				else if (a < b && b != 0)
+				{
+					C.Add(b);
+					C.Add(a);
+				}
+				else if (a > b && a != 0)
+				{
+					A.Add(a);
+					A.Add(b);
+				}
+			}
+			else if (x == 3)
+			{
+				a = A.Get();
+				b = B.Get();
+				if (b == 0)
+					B.Add(a);
+				else if (a == 0)
+					A.Add(b);
+				else if (a < b && b != 0)
+				{
+					B.Add(b);
+					B.Add(a);
+				}
+				else if (a > b && a != 0)
+				{
+					A.Add(a);
+					A.Add(b);
+				}
+			}
+		}
+	}
+};
+
 
 int main()
 {
 	setlocale(0, "");
 	int n;
-	Stek <int> st1, st2, st3;
+	Stek <int> A, B, C;
 	cout << "Введите количество дисков n ";
 	cin >> n;
-	st1.AllAdd(n);
-	st1.Print();
-
-	while(true)
+	if (n <= 0)
 	{
-		if (st2.top == NULL)
-
+		cout << endl << "Дисков нет";
+		system("Pause");
+		return 0;
 	}
+	A.AllAdd(n);
+	A.Print();
+	cout << endl << endl;
 
+	Hannoi(n, A, B, C);
+
+	A.~Stek();
+	B.~Stek();
+	C.~Stek();
 	system("Pause");
     return 0;
 }
--- Stek_Dek_ochered/Stek_Dek_ochered/Stek_template.h
+++ Stek_Dek_ochered/Stek_Dek_ochered/Stek_template.h
@@ -10,6 +10,7 @@ public:
 		entry *next;
 	};
 	entry *top = NULL;
+	int N = 0;
 public:
 	Stek(){}; 
 	void AllAdd(int n) 
@@ -17,6 +18,7 @@ public:
 		for (int i = n; i > 0; i--)
 			Add(i);
 	};
+
 	~Stek() 
 	{
 		while (top != NULL)
@@ -25,10 +27,12 @@ public:
 			top = top->next;
 			delete pr;
 		}
+		N = 0;
 	};
 
 	void Add(Type D)
 	{
+		N++;
 		entry *element = new entry;
 		element->Data = D;
 		element->next = top;
@@ -37,16 +41,24 @@ public:
 
 	Type Get()
 	{
-		Type D = top->Data;
+		if (top == NULL)
+			return N; 
+		N--;
+		/*Type D = top->Data;
 		entry *previous = top->next;
 		delete top;
-		top = previous;
-		return D;
+		entry *top = previous;
+		return D;*/
+		entry *del = top;
+		top = top->next;
+		return del->Data;
+		delete del;
 	};
 
 	void Print()
 	{
 		entry *pr = top;
+		cout << "top -> ";
 		while (pr != NULL)
 		{
 			cout << pr->Data << " ";
